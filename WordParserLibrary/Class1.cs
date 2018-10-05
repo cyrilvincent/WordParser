993bb1e70c057a7d6f1eed8f3555ae954d60a275
cyrilvincent <contact@cyrilvincent.com>
2018-10-05 12:04:53 +0200

    Ajoutez des fichiers projet.

---
+++ WordParserLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordParserLibrary
+{
+    public class Class1
+    {
+    }
+}
